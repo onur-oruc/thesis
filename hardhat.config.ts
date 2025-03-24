776950d1c4bc8289e16e36402251fd74d8deb771
onur-oruc <honouroruc@gmail.com>
2025-03-24 14:18:08 +0100

    chore: Deps and configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
